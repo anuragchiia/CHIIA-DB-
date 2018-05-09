c446ab75ee749ca1f3b84aeedcf8e8cea5652efd
Rajkamal <u6345375@anu.edu.au>
2018-05-09 18:17:48 +1000

    deploy code

---
+++ Code/code/database.py
@@ -0,0 +1,80 @@
+# this file defines the way the database will be setup
+from flask import Flask as fl
+from flask_sqlalchemy import SQLAlchemy
+import os
+import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy as sa
+app = fl(__name__)
+
+#############################################################
+# model support code
+#############################################################
+
+def resetdb():
+    """Destroys and creates the database + tables."""
+
+    metadata = sa.MetaData()
+    metadata.reflect(engine)
+    for tbl in reversed(metadata.sorted_tables):
+        tbl.drop(engine)
+    # if not database_exists(DB_URL):
+    #     print('Creating database.')
+    #     create_database(DB_URL)
+
+    print('Creating tables.')
+    # import the models used to describe the tables we're creating (using the
+    # ORM). Link: http://flask-sqlalchemy.pocoo.org/2.3/models/
+    import models
+    Base.metadata.create_all(bind=engine)
+    db_session.commit()
+    print('Integrating models.')
+
+    # create user object and then commit to db
+    from passlib.hash import sha512_crypt
+    pw_hashed = sha512_crypt.encrypt("admin")
+    pw_hashed_an = sha512_crypt.encrypt("test")
+    new_admin = models.User(fname="Johnny", lname="Admin", email="bcartwri96@gmail.com", language=False, pw_hashed=pw_hashed, admin=True, confirmed=True)
+    new_analyst = models.User(fname="Johny", lname="Test", email="test@test.com", language=True, pw_hashed=pw_hashed_an, admin=False)
+    db_session.add(new_admin)
+    db_session.add(new_analyst)
+    update = models.Admin()
+    new_search_names = models.Search_Names(name="Factiva")
+    update.search_names.append(new_search_names)
+    db_session.add(update)
+    db_session.commit()
+    print("Creating an admin user.")
+    print("Creating an test user.")
+    print("Creating a new search type")
+
+def get_env_variable(name):
+    try:
+        return os.environ[name]
+    except KeyError:
+        message = "Expected environment variable '{}' not set.".format(name)
+        raise Exception(message)
+
+
+#############################################################
+# setup - run python; import database; database.resetdb() to reset db
+#############################################################
+
+# the values of those depend on your setup
+POSTGRES_URL = get_env_variable("POSTGRES_URL")
+POSTGRES_USER = get_env_variable("POSTGRES_USER")
+POSTGRES_PW = get_env_variable("POSTGRES_PW")
+POSTGRES_DB = get_env_variable("POSTGRES_DB")
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+
+# when sending to the REAL WORLD!
+
+
+engine = create_engine(DB_URL, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
---
+++ Code/code/main.py
@@ -0,0 +1,106 @@
+import flask as fl
+from flask_sqlalchemy import SQLAlchemy
+import os
+import controller as con
+from database import db_session
+import database
+import models as ml
+import flask_login as flog
+import datetime
+from flask_bootstrap import Bootstrap
+
+
+app = fl.Flask(__name__)
+lm = flog.LoginManager() # initialise the login lib
+lm.init_app(app) # init the login
+app.secret_key = database.get_env_variable("SECRET_KEY")  # Change this!
+app.permanent_session_lifetime = datetime.timedelta(hours=12)
+
+# main pages
+# ==========
+# below defines the mapping between URI -> controller code
+@app.route('/')
+def index():
+    return con.index()
+
+@app.route('/index')
+def redirect_index():
+    return con.index()
+
+# send /login traffic the login page.
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return con.login()
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    return con.create_account()
+
+@app.route('/confirm_account/<int:id>')
+@flog.login_required
+def confirm_account(id):
+    return con.confirm_account(id)
+
+@app.route("/logout")
+@flog.login_required
+def logout():
+    fl.session.clear()
+    flog.logout_user()
+    return fl.redirect(fl.url_for('index'))
+
+@app.route("/settings", methods=['GET', 'POST'])
+@flog.login_required
+def settings():
+    return con.settings()
+
+# user actions
+# ============
+@app.route("/manage")
+@flog.login_required
+def manage():
+    return con.manage_profile()
+
+@app.route("/edit-user/<int:id>", methods=['GET', 'POST'])
+@flog.login_required
+def edit_user(id):
+    return con.edit_user(id)
+
+@app.route('/delete-user/<int:id>')
+def delete_user(id):
+    return con.delete_user(id)
+
+# datasets
+# ========
+
+@app.route("/dataset/", methods=['GET', 'POST'])
+def manage_datasets():
+    return con.manage_datasets()
+
+@app.route("/dataset/create", methods=['GET', 'POST'])
+def create_dataset():
+    return con.create_dataset()
+
+@app.route("/dataset/edit/<int:id>", methods=['GET', 'POST'])
+def edit_dataset(id):
+    return con.edit_dataset(id)
+
+@app.route("/dataset/delete/<int:id>", methods=['GET', 'POST'])
+def delete_dataset(id):
+    return con.delete_dataset(id)
+
+
+
+# required for login manager
+# ========
+@lm.user_loader
+def load_user(user_id):
+    user = ml.User.query.get(user_id)
+    if user:
+        return user
+    else:
+        return None
+
+# tear down the session iff the app shuts down
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
